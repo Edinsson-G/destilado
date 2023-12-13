ef13992b94c22edac8866e8105cddcc16cf24d03
Edinsson-G <edinssonfernandogutierrez@gmail.com>
2023-12-13 15:45:55 -0500

    ahora el archivo imprimir.py crea un archivo .csv

--- imprimir.py
+++ imprimir.py
@@ -3,14 +3,28 @@ import os
 import pandas as pd
 def impresion_segura(ruta,archivo,indice=None):
     valor=torch.load(ruta+'/'+archivo)if archivo in os.listdir(ruta) else f"{ruta}/{archivo} no existe"
-    if indice!=None and (type(valor)==dict or type(valor)==list):
+    if indice=="min":
+        valor=valor[valor.index(min(valor))]
+    elif indice!=None and (type(valor)==dict or type(valor)==list):
         valor=valor[indice]
+    if torch.is_tensor(valor)and valor.numel()==1:
+        valor=valor.item()
     return valor
 resultados="resultados"
+claves=["carpeta","numClases","accEntr","accTest","accVal","PerdidaMin"]
+tabla={clave:[] for clave in claves}
 for carpeta in os.listdir(resultados):
     ruta=resultados+'/'+carpeta
-    print("carpeta:",carpeta)
-    print("número de clases:",impresion_segura(ruta,"hiperparametros.pt"),"n_classes")
-    print("accuracy de entrenamiento:",impresion_segura(ruta,"accTrainDatosdestilados.pt"),-1)
-    print("accuracy de validación:",impresion_segura(ruta,"accValDatosdestilados.pt"),-1)
-    print("accuracy de testeo:",impresion_segura(ruta,"accTestDatosdestilados.pt"))
+    tabla["carpeta"].append(carpeta)
+    for clave,valor in zip(
+        claves[1:],#el for superior ya iterará las carpetas, no hay que hacerlo aquí
+        [
+            impresion_segura(ruta,"hiperparametros.pt","n_classes"),
+            impresion_segura(ruta,"accTrainDatosdestilados.pt",-1),
+            impresion_segura(ruta,"accTestDatosdestilados.pt",-1),
+            impresion_segura(ruta,"accValDatosdestilados.pt"),
+            impresion_segura(ruta,"histPerdida.pt","min")
+        ]
+    ):
+        tabla[clave].append(valor)
+pd.DataFrame(tabla).to_csv(r"preliminares.csv",index=False)
